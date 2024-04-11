d428e813feca4d5022969555df45e3972366ed55
tranhoaithu <tranthihoaithu1112002@gmail.com>
2024-04-11 22:06:14 +0700

    fix: search and update quantity, remove quantity, feat:á»order

--- epharmacy/pharmacy/views.py
+++ epharmacy/pharmacy/views.py
@@ -3,23 +3,27 @@ import json
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.core.serializers import serialize
 from django.http import JsonResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from rest_framework import viewsets, permissions, generics
+from rest_framework.decorators import api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_jwt.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-from .models import Medicine, User, OrderItem, Cart
+from .models import Medicine, User, OrderItem, Cart, Order, Payment
 from .serializer import MedicineSerializer, UserSerializer, LoginSerializer, OrderSerializer, CartSerializer
 from rest_framework.parsers import MultiPartParser
 
-
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 class RegisterUserAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
@@ -28,8 +32,31 @@ class RegisterUserAPIView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # Tạo payload cho JWT
+            user = serializer.instance
+            payload = jwt_payload_handler(user)
+            # Encode payload để tạo ra JWT
+            token = jwt_encode_handler(payload)
+            # Trả về thông tin người dùng và JWT token
+            return Response({
+                'user': {
+                    'username': user.username,
+                    'email': user.email,
+                },
+                'access_token': token,
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class RegisterUserAPIView(generics.CreateAPIView):
+#     serializer_class = UserSerializer
+#     permission_classes = [AllowAny]
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid():
+#             self.perform_create(serializer)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginUserAPIView(APIView):
@@ -110,25 +137,63 @@ def get_detail_medicine(request, id):
     except Medicine.DoesNotExist:
         return JsonResponse({'error': 'No Medicine matches the given query.'}, status=404)
 
-# @login_required
-# def add_to_cart(request, medicine_id):
-#     try:
-#         medicine = Medicine.objects.get(id=medicine_id)
-#         cart_item, created = Cart.objects.get_or_create(medicine=medicine, user=request.user)
-#         if not created:
-#             cart_item.quantity += 1
-#             cart_item.save()
-#         else:
-#             cart_item.quantity = 1
-#             cart_item.save()
-#         serializer_class = CartSerializer(cart_item)
-#         return JsonResponse(serializer_class.data, status=status.HTTP_201_CREATED)
-#
-#     except Medicine.DoesNotExist:
-#         return JsonResponse({'error': 'No Medicine matches the given query.'}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def checkout(request, username):
+    try:
+        # Tìm người dùng dựa trên username
+        user = User.objects.get(username=username)
+        payment = Payment.objects.first()
+        # Tạo một đơn đặt hàng mới
+        order = Order.objects.create(user=user, payment=payment, status='pending')
+        # Lặp qua các mục trong giỏ hàng và tạo OrderItem cho mỗi mục
+        cart_items = Cart.objects.filter(user=user)
+        total_price = sum(cart_item.medicine.price * cart_item.quantity for cart_item in cart_items)
+        for cart_item in cart_items:
+            order_item = OrderItem.objects.create(
+                order=order,
+                medicine=cart_item.medicine,
+                quantity=cart_item.quantity,
+                price=cart_item.medicine.price * cart_item.quantity
+            )
+        order.total_price = total_price
+        order.save()
+        # Xóa tất cả các mục trong giỏ hàng
+        cart_items.delete()
+
+        return Response({'message': 'Đã tạo đơn hàng thành công.'}, status=status.HTTP_201_CREATED)
+
+    except User.DoesNotExist:
+        return Response({'error': 'Người dùng không tồn tại.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['GET'])
+def get_item_cart(request, username):
+    try:
+        cart_items = Cart.objects.filter(user__username=username)
+        serialized_data = []
+        for cart_item in cart_items:
+            cart_data = CartSerializer(cart_item).data
+            # Lấy thông tin của sản phẩm từ liên kết khóa ngoại
+            item_data = {
+                'medicine_id': cart_item.medicine.id_medicine,
+                'medicine_name': cart_item.medicine.name_medicine,
+                'medicine_price': cart_item.medicine.price,
+                'medicine_img': serialize('python', [cart_item.medicine])[0]['fields'].get("image"),
+
+                # Thêm các thông tin khác của sản phẩm nếu cần thiết
+            }
+            cart_data['medicine'] = item_data
+            serialized_data.append(cart_data)
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    except Cart.DoesNotExist:
+        return Response({'error': 'Không tìm thấy mục giỏ hàng.'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CartViewSet(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         try:
             # fix
@@ -152,11 +217,29 @@ class CartViewSet(APIView):
         except Medicine.DoesNotExist:
             return JsonResponse({'error': 'No Medicine matches the given query.'}, status=status.HTTP_404_NOT_FOUND)
 
+    def delete(self, request, item_id):
+        cart_item = Cart.objects.get(id=item_id)
+        cart_item.delete()
+        return redirect('cart:view_cart')
+
+    def update(self, request):
+        if request.method == 'POST':
+            cart_item_id = request.POST.get('cart_item_id')
+            new_quantity = int(request.POST.get('new_quantity'))
+
+            try:
+                cart_item = Cart.objects.get(id=cart_item_id)
+                cart_item.quantity = new_quantity
+                cart_item.save()
+                return JsonResponse({'success': True})
+            except Cart.DoesNotExist:
+                return JsonResponse({'success': False, 'error': 'Cart item not found'})
+        return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
 
 class OrderViewSet(APIView):
     serializer_class = OrderSerializer
     parser_classes = [MultiPartParser, ]
-
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -177,11 +260,14 @@ class OrderViewSet(APIView):
 
 class SearchResultsView(APIView):
     def get(self, request):
-        query = request.GET.get('q', '')  # Lấy tham số tìm kiếm từ URL
-        all_medicines = Medicine.objects.filter(name_medicine=query)  # Tìm kiếm sản phẩm theo tên
-        medicines_list = []  # Danh sách kết quả
+        query = request.GET.get('q', '')
+        keywords = query.split()
+        conditions = Q()
+        for keyword in keywords:
+            conditions |= Q(name_medicine__icontains=keyword)
 
-        # Format kết quả dưới dạng JSON
+        all_medicines = Medicine.objects.filter(conditions)
+        medicines_list = []
         for medicine in all_medicines:
             medicines_list.append(medicine.to_dict())
 
